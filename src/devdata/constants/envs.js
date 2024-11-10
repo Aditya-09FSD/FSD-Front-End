66893cda39ede03835b7494209a2954dd3f97c09
Ashackq <akashpatelyo2@gmail.com>
2024-11-10 20:06:23 +0530

    new new new build

--- src/devdata/constants/envs.js
+++ src/devdata/constants/envs.js
@@ -1,2 +1,2 @@
-export const apiurl = "http://localhost:3001/api/v1";
-// export const apiurl = "http://3.110.221.81:3001/api/v1";
+// export const apiurl = "http://localhost:3001/api/v1";
+export const apiurl = "http://3.110.221.81:3001/api/v1";
